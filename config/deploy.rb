a5c519125b19129b7cc50d604dabc270103097c9
Makoto Harada <haramako@gmail.com>
2013-02-15 22:10:20 +0900

    .

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,32 +1,35 @@
 require 'bundler/capistrano'
 require 'whenever/capistrano'
 
+role :app, "dorubako.ddo.jp"                          # This may be the same as your `Web` server
+
+
 set :application, "webwalker"
 set :repository,  "git@github.com:haramako/webwalker.git"
 
-# role :web, "localhost"                          # Your HTTP server, Apache/etc
-role :app, "dorubako.ddo.jp"                          # This may be the same as your `Web` server
 set :use_sudo, false
 
 set :deploy_to, "/home/harada/webwalker"
 set :shared_children, %w( tmp log tmp/pids )
+set :public_children, []
 
 set :whenever_roles, [ :app ]
 set :whenever_command, 'bundle exec whenever'
 
+
 namespace :deploy do
 
-  task :restart do
+  task :restart, role: :app do
     run "cd #{deploy_to}/current && bundle exec thin -d restart"
     run "cd #{deploy_to}/current && bundle exec ./walk daemon restart"
   end
 
-  task :start do
+  task :start, role: :app  do
     run "cd #{deploy_to}/current && bundle exec thin -d start"
     run "cd #{deploy_to}/current && bundle exec ./walk daemon start"
   end
 
-  task :stop do
+  task :stop, role: :app do
     run "cd #{deploy_to}/current && bundle exec thin -d stop"
     run "cd #{deploy_to}/current && bundle exec ./walk daemon stop"
   end
